f38bd55cef0496baff21295f32c3820c8f3e43c4
BrandonSelden <brandonselden@hotmail.com>
2019-09-06 12:21:44 -0700

    Look Busy finished

--- app/src/main/java/com/example/uselessmachine/MainActivity.java
+++ app/src/main/java/com/example/uselessmachine/MainActivity.java
@@ -9,7 +9,9 @@ import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.ProgressBar;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.uselessmachine.R;
@@ -18,6 +20,11 @@ public class MainActivity extends AppCompatActivity {
     private Switch use;
     private Button button;
     private ConstraintLayout background;
+    private int colorr;
+    private int colorc;
+    private ProgressBar bar;
+    private TextView busy;
+    private Button busyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +32,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         wireWidgets();
         setListeners();
+        colorr = Color.rgb(203, 34, 40);
+        colorc = Color.rgb(255, 255, 255);
     }
 
     private void wireWidgets() {
         use = findViewById(R.id.switch_main_useless);
         button = findViewById(R.id.button_main_selfdestruct);
+        background = findViewById(R.id.background_main_stuff);
+        bar = findViewById(R.id.progressBar_main_busy);
+        busy = findViewById(R.id.textView_main_busy);
+        busyButton = findViewById(R.id.button2_main_busy);
     }
 
     private void setListeners(){
@@ -40,6 +53,15 @@ public class MainActivity extends AppCompatActivity {
                     int x = 11;
                     public void onTick(long l){
                         x--;
+                        if(x % 2 == 0)
+                        {
+                            button.setBackgroundColor(colorr);
+                            background.setBackgroundColor(colorc);
+                        }
+                        else{
+                            button.setBackgroundColor(colorc);
+                            background.setBackgroundColor(colorr);
+                        }
                         button.setText(x + "");
                     }
                     @Override
@@ -52,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
         use.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, final boolean isChecked) {
-                new CountDownTimer(2000, 1000) {
+                new CountDownTimer(500000, 1000) {
                     @Override
                     public void onTick(long l) {
                         if(!use.isChecked())
@@ -64,6 +86,34 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onFinish() {
                         use.setChecked(false);
+                    }
+                }.start();
+            }
+        });
+        busyButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view){
+                new CountDownTimer(10000, 100){
+                    int yay = 0;
+                    public void onTick(long l){
+                        busy.setVisibility(View.VISIBLE);
+                        busy.setText("Loading Important Stuff...");
+                        busyButton.setVisibility(View.INVISIBLE);
+                        use.setVisibility(View.INVISIBLE);
+                        bar.setVisibility(View.VISIBLE);
+                        button.setVisibility(View.INVISIBLE);
+                        bar.setProgress(yay);
+                        yay++;
+                    }
+                    public void onFinish(){
+                        busy.setVisibility(View.INVISIBLE);
+                        busyButton.setVisibility(View.VISIBLE);
+                        use.setVisibility(View.VISIBLE);
+                        bar.setVisibility(View.INVISIBLE);
+                        button.setVisibility(View.VISIBLE);
+                        yay = 0;
+                        bar.setProgress(yay);
                     }
                 }.start();
             }
